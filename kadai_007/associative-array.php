5227aa613254178cd10bc74baebd716d2f8ea2ec
TaharaMitsuru <92862626+TaharaMitsuru@users.noreply.github.com>
2023-08-10 13:30:52 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <?php
+
+        $result = ['name'=>'onion', 'price'=>'200', 'weight'=>'160'];
+
+        print_r($result);
+
+        ?>
+
+    </body>
+</html>
