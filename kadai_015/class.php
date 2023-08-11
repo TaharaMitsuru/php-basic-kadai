b7b80d0fb337869f458b0b5404cd87c354e49ee9
TaharaMitsuru <92862626+TaharaMitsuru@users.noreply.github.com>
2023-08-11 16:17:10 +0900

    Add files via upload

---
+++ kadai_015/class.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+
+        class Food {
+
+            private $name;
+            private $price;
+
+            public function __construct(string $name, int $price){
+            
+                $this->name = $name;
+                $this->price = $price;
+
+            }
+
+            public function show_price(){
+            echo $this->price;
+            }
+        }
+        
+        class Animal{
+
+            private $name;
+            private $height;
+            private $weight;
+
+            public function __construct(string $name, int $height, int $weight)
+            {
+                $this->name = $name;
+                $this->height = $height;
+                $this->weight = $weight;
+            }
+
+            public function show_height(){
+                echo $this->height;
+            }
+        }
+
+        $food = new Food('スイカ', 450);  
+        $animal = new Animal('ねこ', 30, 6);  
+
+        print_r($food);
+        echo '<br>';
+
+        print_r($animal);
+        echo '<br>';
+
+        $food->show_price();
+        echo '<br>';
+
+        $animal->show_height();
+        echo '<br>';
+        ?>
+    </body>
+</html>
